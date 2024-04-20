6d4e8d41a853ef43fc2e5bffec704231a01097c2
Marco Garcia <118572129+JMarco-Garcia@users.noreply.github.com>
2024-04-20 14:06:11 -0500

    Add files via upload

---
+++ src/sesion4/Sesion4.java
@@ -0,0 +1,20 @@
+package sesion4;
+
+import sesion4.pantallas.PantallaCalculadora;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Sesion4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PantallaCalculadora pantalla = new PantallaCalculadora();
+        pantalla.setVisible(true);
+    }
+    
+}
